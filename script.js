ebd490624de484a547b2c61e20dc811b8558d422
Laman Cabbarova <lyamancbbrva@gmail.com>
2024-03-29 00:24:54 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1.3,
+    centeredSlides: true,
+    spaceBetween: 30,
+    // autoplay: {
+    //   delay: 3000, 
+    //   disableOnInteraction: false, 
+    //   },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
